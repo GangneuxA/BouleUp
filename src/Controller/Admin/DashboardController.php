8f673cbf3d95fb24d3d81ce6c7960c702e15eba9
GangneuxA <gangneux.alex@gmail.com>
2022-11-02 14:36:57 +0100

    modfi date/time

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -8,6 +8,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Contracts\Menu\MenuInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use App\Entity\User;
 use App\Entity\Article;
 use App\Entity\Event;
